cf4ff9863fdda331e8cd5867b030de6c74988333
pratikshinde590 <147705816+pratikshinde590@users.noreply.github.com>
2024-04-25 01:15:17 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,14 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def max_water_area(height):
+    """
+    Calculates the maximum area of water that can be trapped between vertical lines.
+
+    :param height: A list of non-negative integers representing the heights of vertical lines.
+    :type height: List[int]
+    :return: The maximum area of water that can be trapped.
+    :rtype: int
+    """
+    pass
+
     
 
 
